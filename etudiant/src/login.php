e3a34225a6a8d3bf85f0793639155db77278f835
TheAkli <46409005+TheAkli@users.noreply.github.com>
2020-05-02 02:05:08 +0200

    ajout de div class

--- etudiant/src/login.php
+++ etudiant/src/login.php
@@ -15,6 +15,9 @@
 	<a href="https://www.u-cergy.fr/fr/index.html"><img src="../images/logo.jpg" alt="Cergy université" height="100" width="140"></a>
 	</header>
 
+
+<div class="login">
+	<h2 style='text-align: center; border-bottom: 2px solid #570342;'>Login</h2>
 	<form action='./connexion.php' method='POST' id='connect'>
 		<label for='email'>Votre Email :</label>
 		<input type="email" name="email" placeholder="Adresse mail" required><br>
@@ -26,5 +29,6 @@
 		style='border-bottom: 2px solid #570342;'>Se connecter <a href='login.php'>administratif / enseignant(e)</a>
 		</p>
 	</form>
+</div>
 </body>
 </html>
